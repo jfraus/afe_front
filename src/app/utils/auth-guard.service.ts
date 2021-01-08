00357d63e83fba95e3876241cceddbe03e687fb4
Juan Ramirez <juan_x2_ramirez@hdm.honda.com>
2021-01-08 08:38:37 -0600

    menu redirect first to login

--- src/app/utils/auth-guard.service.ts
+++ src/app/utils/auth-guard.service.ts
@@ -1,41 +1,44 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
- 
- 
-@Injectable({providedIn: "root"})
+
+
+@Injectable({ providedIn: "root" })
 export class AuthGuardService implements CanActivate {
- 
-    constructor(private router: Router, public AuthService:AuthService) {        
+
+    constructor(private router: Router, public AuthService: AuthService) {
     }
-    
-    public isLoggedIn():boolean {        
-        return localStorage.getItem('isLoggedIn') == "true" ? true : false;      
-    }    
- 
+
+    public isLoggedIn(): boolean {
+        return localStorage.getItem('isLoggedIn') == "true" ? true : false;
+    }
+
     canActivate(route: ActivatedRouteSnapshot,
-                state: RouterStateSnapshot): boolean {
-        
-        if (!this.isLoggedIn() || !this.checkPermissions(state))  {
+        state: RouterStateSnapshot): boolean {
+
+        if (!this.isLoggedIn() || !this.checkPermissions(state)) {
             //redirect to login/home page etc
             //return false to cancel the navigation
+            if (state.url === "/") {
+                this.router.navigateByUrl('/login');
+                return false;
+            }
             this.router.navigateByUrl('/accessdenied');
             return false;
-        } 
+        }
         return true;
     }
 
-    public checkPermissions(state): boolean{
+    public checkPermissions(state): boolean {
         let menu = JSON.parse(sessionStorage.getItem("menu"));
-        if(state.url === "/"){
+        if (state.url === "/") {
             return true;
-        }else{
-            if(menu.find(element => element.routerLink[0] === state.url)){
+        } else {
+            if (menu.find(element => element.routerLink[0] === state.url)) {
                 return true;
             }
         }
-        sessionStorage.setItem("permRouter","false");
+        sessionStorage.setItem("permRouter", "false");
         return false;
     }
 }
- 
