3cfe634c47563543c08682b56d7545ec637789e0
Antonio Perez <antonio_X_perez@hdm.honda.com>
2021-07-13 12:09:17 -0500

    I added a main view of invoice

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -7,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   })
   export class InvoiceComponent implements OnInit {
 
+    cols = [];
+    invoices = [];
+    loadingInvoice = false;
     
     constructor() { }
 
     ngOnInit() {
+      this.cols = [
+        {field: 'colum', header: 'Plataforma'},
+        {field: 'colum', header: 'Cliente'},
+        {field: 'colum', header: 'No de Viaje'},
+        {field: 'colum', header: 'Type'},
+        {field: 'colum', header: 'Destino'},
+        {field: 'colum', header: 'Total Unidades'},
+        {field: 'colum', header: 'Costo Total '}
+        
+      ]
+
+
 
     }
 
