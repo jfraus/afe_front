0806b5f388d84ee8b36ae12b9e0d9456967adc5b
Antonio Perez <antonio_X_perez@hdm.honda.com>
2021-08-26 15:16:46 -0500

    I added the integration with the backend

--- src/app/components/port-catalog/port-catalog.component.ts
+++ src/app/components/port-catalog/port-catalog.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { port } from 'src/app/models/port.model'
+import { PortControllerService } from 'src/app/services/port-controller-service';
 
 @Component({
   selector: 'app-port-catalog',
   templateUrl: './port-catalog.component.html',
-  styleUrls: ['./port-catalog.component.css']
+  styleUrls: ['./port-catalog.component.css'],
+  providers: [ PortControllerService ]
 })
 export class PortCatalogComponent implements OnInit {
 
@@ -12,14 +14,24 @@ export class PortCatalogComponent implements OnInit {
   loadingPort:boolean = false;
   ports: port[] =[];
 
-  constructor() { }
+  constructor(private portControllerService :PortControllerService) { }
 
   ngOnInit() {
     this.cols = [
-      {field: 'dealerNumber', header: 'Código de Puerto'},
-      {field: 'dealerName', header: 'Descripción de Puerto'},
+      {field: 'portCode', header: 'Código de Puerto'},
+      {field: 'portDescription', header: 'Descripción de Puerto'},
       {field: 'countryName', header: 'País'}
     ]
+    this.getPorts();
+
+  }
+
+  getPorts(){
+    this.loadingPort = true;
+    this.portControllerService.getClients().subscribe(data=>{      
+        this.ports = data;
+        this.loadingPort = false;
+    });
   }
 
   updatePort(port: port){
