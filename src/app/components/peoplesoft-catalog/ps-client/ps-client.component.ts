2c985a299f1b71e28c689129e5e6516f00401e3d
Antonio Perez <antonio_X_perez@hdm.honda.com>
2021-09-15 15:40:27 -0500

    i integrated wit the service

--- src/app/components/peoplesoft-catalog/ps-client/ps-client.component.ts
+++ src/app/components/peoplesoft-catalog/ps-client/ps-client.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { PeoplesoftClient } from 'src/app/models/peoplesoftClient.model'
+import { PeopleSofController } from 'src/app/services/peoplesoft-controller.service'
+
+
 @Component({
   selector: 'app-ps-client',
   templateUrl: './ps-client.component.html',
-  styleUrls: ['./ps-client.component.css']
+  styleUrls: ['./ps-client.component.css'],
+  providers:[PeopleSofController]
 })
 export class PsClientComponent implements OnInit {
 
@@ -14,16 +18,25 @@ export class PsClientComponent implements OnInit {
   
 
 
-  constructor() { }
+  constructor(private peopleSofController :PeopleSofController) { }
 
   ngOnInit() {
     this.cols = [
-      {field: 'claveClient', header: 'Clave Cliente'},
+      {field: 'claveCliente', header: 'Clave Cliente'},
       {field: 'nombreClient', header: 'Nombre Cliente'},
       {field: 'clavePeoplesoft', header: 'Clave Peoplesoft'},
       {field: 'filial', header: 'Filial'}
     ]
+    this.getClients();
+  }
 
+
+  getClients(){
+    this.loadingClient =true;
+    this.peopleSofController.getClients().subscribe(data =>{
+      this.clients = data;
+      this.loadingClient =false;
+    });
   }
 
 
