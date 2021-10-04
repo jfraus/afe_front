a915e557cff8c31a609da459bb4c3560a9b426d3
Antonio Perez <antonio_X_perez@hdm.honda.com>
2021-10-04 11:53:04 -0500

    I added the new view to add and edit maintenance

---
+++ src/app/components/purchase-order/maintenance/add-edit-maintenance/add-edit-maintenance.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-maintenance',
+  templateUrl: './add-edit-maintenance.component.html'
+})
+export class AddEditMaintenanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
