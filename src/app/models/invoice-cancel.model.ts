f1bdc680367a182b15a56771f2b54420cada5008
Diego Arellano Briseño <diego_arellanobriseo@hdm.honda.com>
2021-11-22 09:32:58 -0600

    Create view for download report cancel invoice

---
+++ src/app/models/invoice-cancel.model.ts
@@ -0,0 +1,12 @@
+export class InvoiceCancel {
+    invoiceCancel: string;
+    invoiceNew: string;
+    cancellationType: string;
+    cancellationDate: string;
+    reason: string;
+    noteSeries: string;
+    noteFolio: string;
+    stampDate: string;
+    uuid: string;
+    manualInvoice: string;
+}
