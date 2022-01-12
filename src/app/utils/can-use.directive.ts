007c384e63d98d55fdf7083f08257566f2ce7a2f
Antonio Perez <antonio_X_perez@hdm.honda.com>
2022-01-12 15:00:49 -0600

    I attended a comment and add formart to document

--- src/app/utils/can-use.directive.ts
+++ src/app/utils/can-use.directive.ts
@@ -3,40 +3,39 @@ import { Router } from '@angular/router';
 import { Authority } from './authority.model';
 
 @Directive({
-    selector: '[can-use]'
-  })
-  export class CanUse{
-      
-    @Input() typeAction: string;
+  selector: '[can-use]'
+})
+export class CanUse {
 
-  constructor(private element: ElementRef, 
-              private router: Router) {}
-              
+  @Input() typeAction: string;
+
+  constructor(private element: ElementRef,
+    private router: Router) { }
+
   ngOnInit() {
-    let url= this.router.url.split('/')[1];
-    if(!this.can(url, this.typeAction)){
-     // console.log("Entra aquí ");
+    let url = this.router.url.split('/')[1];
+    if (!this.can(url, this.typeAction)) {
       this.element.nativeElement.style.display = 'none';
     }
   }
 
-  getAuthorities(): Authority[]{
+  getAuthorities(): Authority[] {
     let authorities = localStorage.getItem('authorities');
-    if(authorities === null) {
+    if (authorities === null) {
       return null;
     }
     return JSON.parse(authorities);
   }
 
-  can(view: string, action: string): boolean {    
-    let authorities: Authority[] = this.getAuthorities();    
-    if(authorities === null){
+  can(view: string, action: string): boolean {
+    let authorities: Authority[] = this.getAuthorities();
+    if (authorities === null) {
       return false;
     }
-    let viewAuth = authorities.find(a => 
+    let viewAuth = authorities.find(a =>
       a.authority === view
     );
-    return viewAuth !== undefined ? viewAuth.can[0].indexOf(action) > -1 : false; 
+    return viewAuth !== undefined ? viewAuth.can[0].indexOf(action) > -1 : false;
   }
-  
+
 }
